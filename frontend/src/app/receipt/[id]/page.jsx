8008a6c25399ff4a7ecc4aba724b5fa53bc79fd7
Surajit <surajithalder088@gmail.com>
2025-02-06 20:02:15 +0530

    revires form added

--- frontend/src/app/receipt/[id]/page.jsx
+++ frontend/src/app/receipt/[id]/page.jsx
@@ -11,6 +11,10 @@ const Receipt = () => {
      const receipt=receipts.find((item)=>item.id===Number(id))
 
       const [paid,setPaid]=useState(receipt.paid)
+      const [reviewed,setReviewed]=useState(receipt.paid)
+      const[starvalue,setStarvalue]=useState(0)
+      const [reviewText,setReviewText]=useState("")
+      const starArray=[1,2,3,4,5]
   return (
     <>
     <Navbar/>
@@ -23,12 +27,16 @@ const Receipt = () => {
       <p><div className="variable">service.Type: </div>{receipt.type}</p>
       <p><div className="variable">Provider Name: </div>{receipt.hotelName}</p>
       <p><div className="variable">hotel Registered at : </div>{receipt.hotelRegisterd}</p>
-      </div>
-      <div className="second">
-      <p><div className="variable">Total payable Price: </div>
+      
+        <p><div className="variable">Total payable Price: </div>
       <img src="/rupee-indian.png" className="rupee"/>
       {receipt.price}</p>
       <p><div className="variable">Service Details: </div>{receipt.details}</p>
+      </div>
+      <div className="second">
+    
+      
+      
       {
         !paid?
         <div className="payment">
@@ -46,6 +54,39 @@ const Receipt = () => {
       }
      </div>
       <p><div className="variable">receipt generated at : </div>{receipt.craeteAt}</p>
+     <div className="reviewbox">
+      {
+        !reviewed?<div className="reviewform">
+          <div className="star">
+            {
+              starArray.map(s=>(
+                <img className="starimg" src={(starvalue>=s)?"/star-fill.png":"/star-line.png"} key={s} 
+                onClick={()=>{
+                  setStarvalue(s)
+                  
+                }}
+                />
+              ))
+            }
+            
+            
+          </div>
+         <textarea placeholder="Give reviews" 
+         value={reviewText}
+         onChange={(e)=>setReviewText(e.target.value)}
+         ></textarea>
+          <button
+          onClick={()=>{
+            console.log({reviewText,starvalue});
+            
+          }}
+          className="btn"
+          >Submit</button>
+        </div>:
+        <p>You already reviewed</p>
+      }
+     </div>
+     
       </div>
     </div>
     </>
