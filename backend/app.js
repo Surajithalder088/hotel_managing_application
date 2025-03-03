d17ecd31ec51b8da3b5c157f1cc0975473bc7d37
Surajit <surajithalder088@gmail.com>
2025-03-04 01:23:39 +0530

    stripe styling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config.js"
 const app= express();
 import cors from "cors"
+import Stripe from 'stripe'
 import cookieParser from "cookie-parser";
 import  connectDB  from "./batabase/batabase.js";
 import customerRoute from "./routes/customer.js"
@@ -12,7 +13,7 @@ import reviewRoute from "./routes/review.js"
 
 connectDB();
 
-
+const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -22,6 +23,26 @@ app.use(cors({
     credentials:true,
     methods:"GET,POST,PUT,DELETE"
 }))
+
+
+
+
+// api to createpayment intent
+app.post('/create-payment-intent',async(req,res)=>{
+    const {amount}=req.body
+    try {
+        const paymentIntent=await stripe.paymentIntents.create({
+            amount:100,
+            currency:'usd',
+            payment_method_types:["card"]
+        })
+        res.status(200).json({clientSecret:paymentIntent.client_secret})
+    } catch (error) {
+        res.status(400).json({message:"payment intent failed ",error})
+    }
+
+})
+
 app.use('/api/customer',customerRoute)
 app.use('/api/hotel',hotelRoute)
 app.use('/api/service',serviceRoute)
@@ -30,5 +51,7 @@ app.use('/api/review',reviewRoute)
 app.get("/",(req,res)=>{
     res.send(" working")
 })
+
+
 
 export default app;
